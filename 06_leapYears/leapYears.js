e42e52fa44b513fcfe39618ec20ba9cf9e96d680
whitmank <whitman.karter@gmail.com>
2024-01-07 00:56:18 -0500

    Completed exercises 1 through 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (number) {
+    let isLeapYear = undefined;
+    if (number % 4 == 0) {
+        if ((number % 100 == 0) && (number % 400 != 0)) {
+            isLeapYear = false;
+        } else isLeapYear = true;
+    } else isLeapYear = false;
+    
+    return isLeapYear;
 };
 
 // Do not edit below this line
